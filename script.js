60fff61e4fe8e3e85463f948842f8ba79910a943
阿諏訪泰義 <noasuwa@gmail.com>
2026-07-25 12:05:47 +0900

    FightMemo v0.1

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const fights = [
+  {
+    date: "7月26日",
+    event: "RIZIN LANDMARK",
+    fighters: [
+      "朝倉未来",
+      "平本蓮"
+    ]
+  },
 
+  {
+    date: "8月3日",
+    event: "世界タイトルマッチ",
+    fighters: [
+      "井上尚弥",
+      "対戦相手未定"
+    ]
+  },
+
+  {
+    date: "8月16日",
+    event: "ONE Championship",
+    fighters: [
+      "武尊",
+      "スーパーレック"
+    ]
+  }
+];
