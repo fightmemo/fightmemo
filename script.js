65fd84105ae02d7e38597874add5df2e74dd1052
阿諏訪泰義 <noasuwa@gmail.com>
2026-07-25 12:15:07 +0900

    FightMemo v0.2 UI

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
 const fights = [
   {
-    date: "7月26日",
+    sport: "RIZIN",
+    date: "2026/7/26",
     event: "RIZIN LANDMARK",
-    fighters: [
-      "朝倉未来",
-      "平本蓮"
-    ]
+    fighter1: "朝倉未来",
+    fighter2: "平本蓮",
+    url: "https://jp.rizinff.com/"
   },
-
   {
-    date: "8月3日",
+    sport: "BOXING",
+    date: "2026/8/3",
     event: "世界タイトルマッチ",
-    fighters: [
-      "井上尚弥",
-      "対戦相手未定"
-    ]
+    fighter1: "井上尚弥",
+    fighter2: "対戦相手未定",
+    url: "https://boxingnews.jp/"
   },
-
   {
-    date: "8月16日",
+    sport: "ONE",
+    date: "2026/8/16",
     event: "ONE Championship",
-    fighters: [
-      "武尊",
-      "スーパーレック"
-    ]
+    fighter1: "武尊",
+    fighter2: "スーパーレック",
+    url: "https://www.onefc.com/"
   }
 ];
+
+const list = document.getElementById("fight-list");
+
+fights.forEach(fight => {
+
+  const card = document.createElement("div");
+  card.className = "card";
+
+  let sportClass = "";
+
+  if (fight.sport === "RIZIN") sportClass = "rizin";
+  if (fight.sport === "BOXING") sportClass = "boxing";
+  if (fight.sport === "ONE") sportClass = "one";
+
+  card.innerHTML = `
+    <div class="sport ${sportClass}">${fight.sport}</div>
+
+    <div class="date">${fight.date}</div>
+
+    <div class="event">${fight.event}</div>
+
+    <div class="fighters">
+        <div>${fight.fighter1}</div>
+        <div class="vs">VS</div>
+        <div>${fight.fighter2}</div>
+    </div>
+  `;
+
+  card.onclick = () => {
+    window.open(fight.url, "_blank");
+  };
+
+  list.appendChild(card);
+
+});
